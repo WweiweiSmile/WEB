7c3a0192fb33aad5540d6cedb7812f7316c6e203
SlightSmiled <43466487+SlightSmiled@users.noreply.github.com>
2020-09-30 18:16:56 +0800

    不知道更改了什么

--- test-code/test-code.js
+++ test-code/test-code.js
@@ -1,17 +1,39 @@
-console.log(a); //输出什么
-console.log(b); //输出什么
-var a = function() {};
-function b() {}
-
-
-
-function test() {
-	function aaa() {
-		console.log(this);
+const data = { a: 1, b: 'test' };
+const tmpl = 'this is a = {{ a }}, this is b = {{ b }}';
+console.log(tmpllat(tmpl, data));
+function tmpllat(tmpl, data) {
+	let tmplXB = [];
+	let tmplInfo = [];
+	let tmplStr = '';
+	// 找到模版字符串，并将相关的信息 保存到tmplInfo数组中
+	for (let i = 0; i < tmpl.length; i++) {
+		if (tmpl[i] === '{' && i + 1 < tmpl.length && tmpl[i + 1] === '{') {
+			tmplXB.push(i);
+		}
+		if (tmpl[i] === '}' && i + 1 < tmpl.length && tmpl[i + 1] === '}') {
+			let start = tmplXB.pop();
+			let temp = tmpl.slice(start + 2, i).trim();
+			tmplInfo.push({
+				str: data[temp],
+				start: start,
+				end: i + 1
+			});
+		}
 	}
-	this.aaa = aaa;
+	// 如果没有模版字符串 直接返回
+	if (tmplInfo.length === 0) {
+		return tmpl;
+	}
+	// 如果有模版字符串，开始替换
+	for (let i = 0, start = 0; i < tmplInfo.length; i++) {
+		let temp = tmplInfo[i];
+		tmplStr += tmpl.slice(start, temp.start);
+		tmplStr += temp.str;
+		start = temp.end + 1;
+		// 如果这是最后一个模版字符串，将后面的字符串加起来
+		if (i + 1 === tmplInfo.length) {
+			tmplStr += tmpl.slice(start, tmpl.length);
+		}
+	}
+	return tmplStr;
 }
-var object = new test();
-object.aaa(); //输出什么  为什么
-var aaa = object.aaa;
-aaa(); //输出什么  为什么
